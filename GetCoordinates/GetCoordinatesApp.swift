1a0406301cd0522dd6aadffd15ef1134cf4a1ec6
id2k1149 <id2k1149@gmail.com>
2023-05-16 11:39:41 -0400

    Initial Commit

---
+++ GetCoordinates/GetCoordinatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetCoordinatesApp.swift
+//  GetCoordinates
+//
+//  Created by Max Franz Immelmann on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct GetCoordinatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
