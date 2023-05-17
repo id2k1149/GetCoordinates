56db7da1d91b638d7c6fcbf8e9e2c27b1ced5cc5
id2k1149 <id2k1149@gmail.com>
2023-05-17 10:55:37 -0400

    add index and delay

--- GetCoordinates/PathView.swift
+++ GetCoordinates/PathView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct PathView: View {
     @Binding var progress: Double
+    @State private var currentIndex = 0
     
     var body: some View {
+        
         let diameter = UIScreen.main.bounds.width * 0.9
         let radius = diameter / 2
         let points = 5
@@ -22,32 +24,40 @@ struct PathView: View {
             return (x: dotX, y: dotY)
         }
         
-        return VStack {
-            ZStack {
-
-                ForEach(Array(0..<points), id: \.self) { iteration in
-                    let coordinate1 = coordinates[iteration]
-                    let coordinate2 = coordinates[(iteration + 2) % points]
-                    let x1 = coordinate1.x
-                    let y1 = coordinate1.y
-                    let x2 = coordinate2.x
-                    let y2 = coordinate2.y
-                    
-                    Path { path in
-                        path.move(to: CGPoint(x: x1, y: y1))
-                        path.addLine(to: CGPoint(x: x2, y: y2))
-                    }
-                    .trim(from: 0.0, to: CGFloat(progress))
-                    .stroke(Color.black, lineWidth: 2)
-                    .animation(.easeInOut(duration: 1), value: progress)
+        return ZStack {
+            let coordinate1 = coordinates[currentIndex]
+            let coordinate2 = coordinates[(currentIndex + 2) % points]
+            let x1 = coordinate1.x
+            let y1 = coordinate1.y
+            let x2 = coordinate2.x
+            let y2 = coordinate2.y
+            
+            Path { path in
+                path.move(to: CGPoint(x: x1, y: y1))
+                path.addLine(to: CGPoint(x: x2, y: y2))
+            }
+            .trim(from: 0.0, to: CGFloat(progress))
+            .stroke(Color.black, lineWidth: 2)
+            .animation(.easeInOut(duration: 1), value: progress)
+            
+        }
+        .frame(width: diameter, height: diameter)
+        .onAppear {
+            displayNextItem()
+            
+            func displayNextItem() {
+                guard currentIndex < coordinates.count - 1 else {
+                    return
+                }
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    currentIndex += 1
+                    displayNextItem()
                 }
             }
-            .frame(width: diameter, height: diameter)
         }
     }
 }
-
-
 
 struct PathView_Previews: PreviewProvider {
     static var previews: some View {
